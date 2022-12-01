b09b12acccabdbeebba843cb726bc451724a1656
Murilo Analiel Machado <Murilo@selettra.local>
2022-11-30 21:56:36 -0300

    aula 1 e 2, móidulo 2 formação js para front

---
+++ 1 - JavaScript manipulando o DOM/js/main.js
@@ -0,0 +1,10 @@
+const robotronImg = document.querySelector('.robotron');
+robotronImg.addEventListener("click", () => {
+  alert('Robotron says Hello!')
+});
+
+function sayHello(initialize){
+    console.log('Hello, welcome to Robotron 2000')
+}
+
+sayHello()
