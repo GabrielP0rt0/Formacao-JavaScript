0959b388608d2189b01d110d710dd21bc52941b6
Murilo Analiel Machado <Murilo@selettra.local>
2022-12-03 18:47:28 -0300

    exercicio de tintas

--- 1 - JavaScript manipulando o DOM/js/main.js
+++ 1 - JavaScript manipulando o DOM/js/main.js
@@ -1,4 +1,4 @@
-const elementosControleAjuste = document.querySelectorAll(".controle-ajuste")
+/*const elementosControleAjuste = document.querySelectorAll("[data-controle]")
 
 elementosControleAjuste.forEach( (elemento) => {
   elemento.addEventListener('click', (evento) => {
@@ -7,15 +7,33 @@ elementosControleAjuste.forEach( (elemento) => {
 })
 
 function ManipulaDados (operacao, controle){
-  const peca = controle.querySelector(".controle-contador")
+  const peca = controle.querySelector("[data-contador]")
+  const idPeca = peca.id
 
   if (operacao === "+"){
     peca.value =   parseInt(peca.value) + 1
-    console.log("soma")
   } else if (operacao === "-" && parseInt(peca.value) > 0 ){
     peca.value = parseInt(peca.value) - 1
   } else {
-    alert("Apenas núeros positivos de braços")
+    alert("Apenas número positivo de " + idPeca)
   }
-}
+}*/
 
+const botaoMostra = document.querySelectorAll("[data-controle]")
+
+botaoMostra.forEach( (elemento) => {
+  elemento.addEventListener('click', (evento) => {
+    EditaTintas(evento.target.textContent)
+  })
+})
+
+function EditaTintas(funcao){
+  if (funcao === "Mostrar tintas:"){
+    console.log("exibe")
+  } else if (funcao === "Esconder tintas:"){
+    console.log("oculta")
+  } else {
+    throw Error("Parametro de função inválido")
+  }
+
+}
